4712c9c2240df5c3a45a892de4035e7ae562e717
paulohenriquemendes <p-h-m@outlook.com>
2026-05-02 18:52:07 -0300

    Refine symbol color and mark usage

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,9 +1,7 @@
 export default function BrandLogo({ compact = false, className = '' }) {
   return (
     <span className={`inline-flex items-center gap-3 ${className}`} aria-label="Paulo Henrique Mendes">
-      <span className="grid h-11 w-11 shrink-0 place-items-center rounded-full border border-paper/20 bg-white p-2">
-        <img className="h-8 w-8 object-contain" src="/brand/simbolo.svg" alt="" aria-hidden="true" />
-      </span>
+      <img className={`${compact ? 'h-10' : 'h-11'} w-auto shrink-0 object-contain`} src="/brand/simbolo.svg" alt="" aria-hidden="true" />
       {!compact && <img className="h-6 w-auto max-w-[210px] object-contain sm:h-7" src="/brand/logotipo.svg" alt="Paulo Henrique Mendes" />}
     </span>
   )
